f1e9f9495f98b7f55a1da84912ea44a749bb1d8c
darasi <zuradaraselia2@gmail.com>
2018-02-14 14:57:57 +0400

    serv

--- src/config/app.js
+++ src/config/app.js
@@ -5,7 +5,7 @@ export default {
   APP_ENV: env('APP_ENV'),
   APP_VERSION: env('APP_VERSION'),
   APP_HOST: env('APP_HOST', '0.0.0.0'),
-  APP_PORT: env('APP_ENV') === 'test' ? env('TEST_APP_PORT') : env('APP_PORT', '3000'),
+  APP_PORT: env('APP_ENV') === 'test' ? env('TEST_APP_PORT') : env('APP_PORT', '8000'),
   APP_TIMEZONE: 'UTC',
   APP_PAGE_LIMIT: env('APP_PAGE_LIMIT', 10)
 };
